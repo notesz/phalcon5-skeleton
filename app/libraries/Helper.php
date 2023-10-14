e1b053054653b5ec6834c8a6e1c67efef1698fe7
notesz <notesz@notesz.net>
2023-10-14 23:42:21 +0200

    Added filestorage service

--- app/libraries/Helper.php
+++ app/libraries/Helper.php
@@ -95,4 +95,48 @@ class Helper
 
         return $manifest['assets/phalcon-skeleton/' . $assetFile] ?? null;
     }
+
+    /**
+     * Get filesize format
+     *
+     * @param integer $bytes
+     * @return float|int|string
+     */
+    public static function getFileSizeFormat($bytes)
+    {
+        $bytes = \floatval($bytes);
+
+        $arBytes = [
+            0 => [
+                'UNIT'  => 'TB',
+                'VALUE' => pow(1024, 4)
+            ],
+            1 => [
+                'UNIT'  => 'GB',
+                'VALUE' => pow(1024, 3)
+            ],
+            2 => [
+                'UNIT'  => 'MB',
+                'VALUE' => pow(1024, 2)
+            ],
+            3 => [
+                'UNIT'  => 'KB',
+                'VALUE' => 1024
+            ],
+            4 => [
+                'UNIT'  => 'B',
+                'VALUE' => 1
+            ],
+        ];
+
+        foreach ($arBytes as $arItem) {
+            if ($bytes >= $arItem['VALUE']) {
+                $result = $bytes / $arItem['VALUE'];
+                $result = \str_replace('.', ',', \strval(round($result, 2))) . ' ' . $arItem['UNIT'];
+                break;
+            }
+        }
+
+        return $result;
+    }
 }
